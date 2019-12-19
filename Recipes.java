50743e2869300bcb49c64ee2140ccf4b662e69a5
AmanOfficialGit <59055062+AmanOfficialGit@users.noreply.github.com>
2019-12-19 22:25:46 +0530

    branch 1

--- Recipes.java
+++ Recipes.java
@@ -6,5 +6,8 @@ System.out.println("Fries");
 System.out.println("Donats");
 System.out.println("Burgur");
 System.out.println("Chicken");
+  
+System.out.println("noodles");
+System.out.println("fish");
 }
 }
